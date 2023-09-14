2ba1705a3c0993dab60dfc1a22fc410305bebe5f
shatterstar99 <32302069+shatterstar99@users.noreply.github.com>
2023-09-14 17:29:58 +0200

    changed hearts to toggle manually

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
     Signalstyrka = radio.receivedPacket(RadioPacketProperty.SignalStrength)
 })
+function Toggleheart () {
+    led.toggle(1, 0)
+    led.toggle(3, 0)
+    led.toggle(0, 1)
+    led.toggle(1, 1)
+    led.toggle(2, 1)
+    led.toggle(3, 1)
+    led.toggle(4, 1)
+    led.toggle(0, 2)
+    led.toggle(1, 2)
+    led.toggle(2, 2)
+    led.toggle(3, 2)
+    led.toggle(4, 2)
+    led.toggle(1, 3)
+    led.toggle(2, 3)
+    led.toggle(3, 3)
+    led.toggle(2, 4)
+}
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showNumber(Score)
@@ -15,7 +33,7 @@ radio.setGroup(115)
 Signalstyrka = 0
 Score = 0
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    Toggleheart()
     basic.pause((0 - (Signalstyrka - 40)) ** 2)
     basic.clearScreen()
     basic.pause((0 - (Signalstyrka - 40)) ** 2)
